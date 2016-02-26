c9243cac1a4099a0b17b162c00f4846473edfea8
Matthieu Barba <mbarba@ebi.ac.uk>
2016-02-26 16:28:05 +0000

    Add an example script to add runs in the database
    This script is a simple wrapper around the RNAseqDB method add_run(). It
    will serve as a demonstration for how the database is populated.

---
+++ scripts/add_SRA_run.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Readonly;
+use Carp;
+use autodie;
+use English qw( -no_match_vars );
+use Getopt::Long qw(:config no_ignore_case);
+
+use RNAseqDB::DB;
+use Log::Log4perl qw( :easy );
+Log::Log4perl->easy_init($DEBUG);
+
+
+###############################################################################
+# MAIN
+# Get command line args
+my %opt = %{ opt_check() };
+
+# Connect to the database schema
+my $db = RNAseqDB::DB->connect(
+  "dbi:mysql:host=$opt{host}:port=$opt{port}:database=$opt{db}",
+  $opt{user},
+  $opt{password}
+);
+
+# Add the SRA run(s) data to the database
+$db->add_run( $opt{run} );
+
+###############################################################################
+# Parameters and usage
+# Print a simple usage note
+sub usage {
+  my $error = shift;
+  my $help = '';
+  if ($error) {
+    $help = "[ $error ]\n";
+  }
+  $help .= <<'EOF';
+    This script adds an SRA run to the RNAseqDB.
+
+    Database connection:
+    --host    <str>   : host name
+    --port    <int>   : port
+    --user    <str>   : user name
+    --password <str>  : password
+    --db <str>        : database name
+    
+    --run <str>       : SRA run identifier (e.g. SRR000000)
+EOF
+  print STDERR "$help\n";
+  exit(1);
+}
+
+# Get the command-line arguments and check for the mandatory ones
+sub opt_check {
+  my %opt = ();
+  GetOptions(\%opt,
+    "host=s",
+    "port=i",
+    "user=s",
+    "password=s",
+    "db=s",
+    "run=s",
+    "help",
+  );
+
+  usage()              if $opt{help};
+  usage("Need --host") if not $opt{host};
+  usage("Need --port") if not $opt{port};
+  usage("Need --user") if not $opt{user};
+  usage("Need --db") if not $opt{db};
+  usage("Need --run") if not $opt{run};
+  $opt{password} ||= '';
+  return \%opt;
+}
+
+__END__
+
